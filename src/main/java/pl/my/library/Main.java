48fbfcf818d24585f23fd6f6643ff1b84419ab83
Tomasz Budny <tombud93@gmail.com>
2017-04-07 14:13:33 +0200

    first commit for my library project

---
+++ src/main/java/pl/my/library/Main.java
@@ -0,0 +1,31 @@
+package pl.my.library;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+/**
+ * Created by Admin on 2017-04-06.
+ */
+public class Main extends Application {
+
+    public static void main(String[] args) {
+     launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/BorderPaneMain.fxml"));
+        BorderPane borderPane = loader.load();
+        Scene scene = new Scene(borderPane);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("My Library");
+        primaryStage.show();
+
+    }
+
+
+
+}
---
+++ src/main/java/pl/my/library/controllers/MainController.java
@@ -0,0 +1,7 @@
+package pl.my.library.controllers;
+
+/**
+ * Created by Admin on 2017-04-06.
+ */
+public class MainController {
+}

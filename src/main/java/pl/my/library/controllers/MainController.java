8fb3a2aa9ef1796865a294b0ec2886e0f9ad94db
Tomasz Budny <tombud93@gmail.com>
2017-04-09 13:39:00 +0200

    bundles, languages, toggle button

--- src/main/java/pl/my/library/controllers/MainController.java
+++ src/main/java/pl/my/library/controllers/MainController.java
@@ -1,7 +1,22 @@
 package pl.my.library.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+
 /**
  * Created by Admin on 2017-04-06.
  */
 public class MainController {
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private TopMenuButtonsController topMenuButtonsController;
+
+    @FXML
+    private void initialize(){
+    topMenuButtonsController.setMainController(this);
+    }
+
 }

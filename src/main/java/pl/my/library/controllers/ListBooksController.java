bf9c5cef4bec66bcdf3ddf39952ccb0da9bc3498
Tomasz Budny <tombud93@gmail.com>
2017-06-03 21:01:05 +0200

    FXML list of books

---
+++ src/main/java/pl/my/library/controllers/ListBooksController.java
@@ -0,0 +1,13 @@
+package pl.my.library.controllers;
+
+import javafx.fxml.FXML;
+
+/**
+ * Created by Admin on 2017-06-03.
+ */
+public class ListBooksController {
+
+    @FXML
+    void initialize(){
+    }
+}
